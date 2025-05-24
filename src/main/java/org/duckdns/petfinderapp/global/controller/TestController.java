1f4edd7e8819abe2ffc0597f77b0fb0b53ed12e3
KJH <fighter0628@gmail.com>
2025-05-24 17:41:05 +0900

    ✨ Feat[#5]: 테스트용 엔드포인트·리포지토리 추가

--- src/main/java/org/duckdns/petfinderapp/global/controller/TestController.java
+++ src/main/java/org/duckdns/petfinderapp/global/controller/TestController.java
@@ -1,9 +1,12 @@
 package org.duckdns.petfinderapp.global.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.duckdns.petfinderapp.domain.user.entity.User;
 import org.duckdns.petfinderapp.global.error.exception.NotFoundException;
 import org.duckdns.petfinderapp.global.template.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +38,13 @@ public class TestController {
         TestException() {
             super("hello");
         }
+    }
+
+    @GetMapping("/user")
+    public ApiResponse<User> testUser(@AuthenticationPrincipal User user) {
+        return ApiResponse.onSuccess(
+                HttpStatus.OK,
+                "정상적으로 호출되었습니다.",
+                user);
     }
 }
