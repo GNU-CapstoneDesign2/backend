9d78063249973d44feb0541215799ba9bc1472f4
KJH <fighter0628@gmail.com>
2025-05-25 11:25:00 +0900

    ✨ Fea[#9]: REST API에 접근 거부 및 인증 실패 핸들러 추가

---
+++ src/main/java/org/duckdns/petfinderapp/global/security/RestAccessDeniedHandler.java
@@ -0,0 +1,19 @@
+package org.duckdns.petfinderapp.global.security;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class RestAccessDeniedHandler implements AccessDeniedHandler {
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException {
+		response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근이 거부되었습니다.");
+	}
+}
