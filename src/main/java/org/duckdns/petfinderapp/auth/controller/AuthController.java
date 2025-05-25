c0ae026694155f318fed587f097d18f255cfc897
KJH <fighter0628@gmail.com>
2025-05-25 11:30:02 +0900

    ✨ Faet[#9]: AuthController에 logout endpoint 추가

--- src/main/java/org/duckdns/petfinderapp/auth/controller/AuthController.java
+++ src/main/java/org/duckdns/petfinderapp/auth/controller/AuthController.java
@@ -5,6 +5,8 @@ import org.duckdns.petfinderapp.auth.service.AuthService;
 import org.duckdns.petfinderapp.global.template.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,20 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("")
+@RequestMapping("/auth")
 public class AuthController {
 	private final AuthService authService;
-	@GetMapping("/auth/login/kakao")
+
+	@GetMapping("/login/kakao")
 	public ApiResponse<LoginResponse> kakaoLogin(@RequestParam("code") String code) {
 		LoginResponse data = authService.kakaoLogin(code);
 		return ApiResponse.onSuccess(HttpStatus.OK, "성공", data);
+	}
+
+	@PostMapping("/logout")
+	public ApiResponse<Void> kakaoLogout(@RequestHeader("Authorization") String bearer) {
+		String token = bearer.replace("Bearer ", "");
+		authService.logout(token);
+		return ApiResponse.onSuccess(HttpStatus.NO_CONTENT, "로그아웃 성공", null);
 	}
 }
