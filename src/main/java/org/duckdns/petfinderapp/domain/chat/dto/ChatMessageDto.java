2f8fc215c23f3bb23b3cb81826352875a44f7c60
Algorithm <fighter0628@gmail.com>
2025-05-26 15:28:27 +0900

    ✨ Feat[#14]: ChatMessageDto 및 PostInfoDto 클래스에 생성일 및 읽음 상태 추가

--- src/main/java/org/duckdns/petfinderapp/domain/chat/dto/ChatMessageDto.java
+++ src/main/java/org/duckdns/petfinderapp/domain/chat/dto/ChatMessageDto.java
@@ -1,16 +1,31 @@
 package org.duckdns.petfinderapp.domain.chat.dto;
 
+import lombok.Builder;
+import org.duckdns.petfinderapp.domain.chat.entity.ChatMessage;
 import org.duckdns.petfinderapp.domain.post.dto.PostInfoDto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDateTime;
+
+@Builder
 public record ChatMessageDto(
 	@NotNull
 	Long senderId,
 	@NotBlank
 	String message,
-
-	PostInfoDto post
+	LocalDateTime createdAt,
+	PostInfoDto post,
+	Boolean isRead
 ) {
+	public static ChatMessageDto of(ChatMessage chatMessage, PostInfoDto postInfoDto) {
+		return ChatMessageDto.builder()
+			.senderId(chatMessage.getSender().getId())
+			.message(chatMessage.getContent())
+			.createdAt(chatMessage.getCreateAt())
+			.post(postInfoDto)
+			.isRead(chatMessage.getRead())
+			.build();
+	}
 }
--- src/main/java/org/duckdns/petfinderapp/domain/post/dto/PostInfoDto.java
+++ src/main/java/org/duckdns/petfinderapp/domain/post/dto/PostInfoDto.java
@@ -1,14 +1,29 @@
 package org.duckdns.petfinderapp.domain.post.dto;
 
+import lombok.Builder;
+import org.duckdns.petfinderapp.domain.post.entity.PostCommon;
+import org.duckdns.petfinderapp.domain.post.enums.PostState;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record PostInfoDto (
 	Long postId,
-	String state,
+	PostState state,
 	LocalDateTime date,
 	String address,
 	String description,
 	String imageUrl
 ) {
 
+    public static PostInfoDto of(PostCommon post) {
+		return PostInfoDto.builder()
+			.postId(post.getId())
+			.state(post.getState())
+			.date(post.getCreateAt())
+			.address(post.getAddress())
+			.description(post.getDescription())
+			.imageUrl(post.getImages().get(0).getImageUrl())
+			.build();
+    }
 }
