53473abeadddd61373691413472b9091f2eeac82
KJH <fighter0628@gmail.com>
2025-06-07 22:40:53 +0900

    ✨ Feat[#22]: AdoptRepository 인터페이스 추가 및 PostRepository에 @Repository 어노테이션 추가

---
+++ src/main/java/org/duckdns/petfinderapp/domain/post/repository/AdoptRepository.java
@@ -0,0 +1,10 @@
+package org.duckdns.petfinderapp.domain.post.repository;
+
+import java.util.List;
+
+import org.duckdns.petfinderapp.domain.post.entity.Adopt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdoptRepository extends JpaRepository<Adopt, Long> {
+	List<Adopt> findAllByAnimalNumIn(List<String> animalNums);
+}
