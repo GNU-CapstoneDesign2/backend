5a7554cdc47b75e13c863ea03e9d261b59e39d14
Algorithm <fighter0628@gmail.com>
2025-06-10 13:52:13 +0900

    ✨ Feat[#45]: 사용자의 게시글 목록 조회 기능 구현

--- src/main/java/org/duckdns/petfinderapp/domain/post/repository/PostRepository.java
+++ src/main/java/org/duckdns/petfinderapp/domain/post/repository/PostRepository.java
@@ -3,6 +3,7 @@ package org.duckdns.petfinderapp.domain.post.repository;
 import java.util.List;
 
 import org.duckdns.petfinderapp.domain.post.entity.PostCommon;
+import org.duckdns.petfinderapp.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,15 +12,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<PostCommon, Long>, JpaSpecificationExecutor<PostCommon> {
-    List<PostCommon> findAllByOrderByIdDesc();
+public interface PostRepository extends JpaRepository<PostCommon, Long>,
+    JpaSpecificationExecutor<PostCommon> {
 
-    @Query(
-        value = "select p from PostCommon p"
-            + " where treat(p as Adopt).petNum = :petNum"
-            + " or treat(p as Lost).petNum = :petNum",
-        countQuery = "select count(p) from PostCommon p"
-            + " where treat(p as Adopt).petNum = :petNum"
-            + " or treat(p as Lost).petNum = :petNum")
-    Page<PostCommon> findAdoptOrLostByPetNum(String petNum, Pageable pageable);
+  List<PostCommon> findAllByOrderByIdDesc();
+
+  @Query(
+      value = "select p from PostCommon p"
+          + " where treat(p as Adopt).petNum = :petNum"
+          + " or treat(p as Lost).petNum = :petNum",
+      countQuery = "select count(p) from PostCommon p"
+          + " where treat(p as Adopt).petNum = :petNum"
+          + " or treat(p as Lost).petNum = :petNum")
+  Page<PostCommon> findAdoptOrLostByPetNum(String petNum, Pageable pageable);
+
+  Page<PostCommon> findAllByUser(User user, Pageable pageable);
 }
--- src/main/java/org/duckdns/petfinderapp/domain/user/controller/UserController.java
+++ src/main/java/org/duckdns/petfinderapp/domain/user/controller/UserController.java
@@ -1,10 +1,14 @@
 package org.duckdns.petfinderapp.domain.user.controller;
 
+import org.duckdns.petfinderapp.domain.post.dto.response.PostSummaryResponse;
+import org.duckdns.petfinderapp.domain.post.service.PostService;
 import org.duckdns.petfinderapp.domain.user.dto.request.UserUpdateRequest;
 import org.duckdns.petfinderapp.domain.user.dto.response.UserInfoResponse;
 import org.duckdns.petfinderapp.domain.user.entity.User;
 import org.duckdns.petfinderapp.domain.user.service.UserService;
 import org.duckdns.petfinderapp.global.template.ApiResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
@@ -21,35 +25,49 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-	private final UserService userService;
 
-	@GetMapping("/me")
-	public ApiResponse<UserInfoResponse> getCurrentUser(@AuthenticationPrincipal User user) {
-		return ApiResponse.onSuccess(
-			HttpStatus.OK,
-			"내 정보 조회 성공",
-			UserInfoResponse.of(user)
-		);
-	}
+  private final UserService userService;
+  private final PostService postService;
 
-	@PutMapping("/me")
-	public ApiResponse<UserInfoResponse> updateUser(
-		@AuthenticationPrincipal User user,
-		@Validated @RequestBody UserUpdateRequest userUpdateRequest
-	) {
-		UserInfoResponse data = userService.updateUserInfo(user, userUpdateRequest);
-		return ApiResponse.onSuccess(
-			HttpStatus.OK,
-			"내 정보 수정 성공",
-			data
-		);
-	}
+  @GetMapping("/me")
+  public ApiResponse<UserInfoResponse> getCurrentUser(@AuthenticationPrincipal User user) {
+    return ApiResponse.onSuccess(
+        HttpStatus.OK,
+        "내 정보 조회 성공",
+        UserInfoResponse.of(user)
+    );
+  }
 
-	@DeleteMapping("/me")
-	public ApiResponse<Void> deleteUser(
-		@AuthenticationPrincipal User user
-	) {
-		userService.deleteUser(user);
-		return ApiResponse.onSuccess(HttpStatus.NO_CONTENT, "회원 탈퇴 성공", null);
-	}
+  @PutMapping("/me")
+  public ApiResponse<UserInfoResponse> updateUser(
+      @AuthenticationPrincipal User user,
+      @Validated @RequestBody UserUpdateRequest userUpdateRequest
+  ) {
+    UserInfoResponse data = userService.updateUserInfo(user, userUpdateRequest);
+    return ApiResponse.onSuccess(
+        HttpStatus.OK,
+        "내 정보 수정 성공",
+        data
+    );
+  }
+
+  @DeleteMapping("/me")
+  public ApiResponse<Void> deleteUser(
+      @AuthenticationPrincipal User user
+  ) {
+    userService.deleteUser(user);
+    return ApiResponse.onSuccess(HttpStatus.NO_CONTENT, "회원 탈퇴 성공", null);
+  }
+
+  @GetMapping("/me/posts")
+  public ApiResponse<Page<PostSummaryResponse>> getMyPosts(
+      @AuthenticationPrincipal User user,
+      Pageable pageable) {
+    Page<PostSummaryResponse> data = postService.getMyPosts(user, pageable);
+    return ApiResponse.onSuccess(
+        HttpStatus.OK,
+        "내 게시글 조회 성공",
+        data
+    );
+  }
 }
