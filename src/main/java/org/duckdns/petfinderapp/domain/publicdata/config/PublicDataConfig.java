3bed057e48efa91d828aab019245d193da9f9de1
Algorithm <fighter0628@gmail.com>
2025-06-12 00:13:32 +0900

    :bug: Fix[#56]: 캐시 무효화를 위한 publicDataApiWebClient 메서드에 캐시관련 HTTP 헤더 추가

--- src/main/java/org/duckdns/petfinderapp/domain/publicdata/config/PublicDataConfig.java
+++ src/main/java/org/duckdns/petfinderapp/domain/publicdata/config/PublicDataConfig.java
@@ -14,48 +14,51 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PublicDataConfig {
 
-	private static final int MAX_BUFFER_SIZE = 2 * 1024 * 1024; // 2MB
-	private final SgisProperties sgisProperties;
-	private final PublicDataProperties publicDataProperties;
+  private static final int MAX_BUFFER_SIZE = 2 * 1024 * 1024; // 2MB
+  private final SgisProperties sgisProperties;
+  private final PublicDataProperties publicDataProperties;
 
-	/**
-	 * PublicData(유기동물 조회 등) 전용 WebClient
-	 */
-	@Bean
-	public WebClient publicDataApiWebClient(WebClient.Builder builder) {
-		DefaultUriBuilderFactory factory =
-			new DefaultUriBuilderFactory(publicDataProperties.baseUrl());
-		factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+  /**
+   * PublicData(유기동물 조회 등) 전용 WebClient
+   */
+  @Bean
+  public WebClient publicDataApiWebClient(WebClient.Builder builder) {
+    DefaultUriBuilderFactory factory =
+        new DefaultUriBuilderFactory(publicDataProperties.baseUrl());
+    factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
 
-		return builder
-			.uriBuilderFactory(factory)
-			.baseUrl(publicDataProperties.baseUrl())
-			.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-			.exchangeStrategies(ExchangeStrategies.builder()
-				.codecs(configurer ->
-					configurer.defaultCodecs()
-						.maxInMemorySize(MAX_BUFFER_SIZE)
-				)
-				.build()
-			)
-			.build();
-	}
+    return builder
+        .uriBuilderFactory(factory)
+        .baseUrl(publicDataProperties.baseUrl())
+        .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+        .defaultHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate")
+        .defaultHeader(HttpHeaders.PRAGMA, "no-cache")
+        .defaultHeader(HttpHeaders.EXPIRES, "0")
+        .exchangeStrategies(ExchangeStrategies.builder()
+            .codecs(configurer ->
+                configurer.defaultCodecs()
+                    .maxInMemorySize(MAX_BUFFER_SIZE)
+            )
+            .build()
+        )
+        .build();
+  }
 
-	/**
-	 * SGIS API 전용 WebClient
-	 */
-	@Bean
-	public WebClient sgisApiWebClient(WebClient.Builder builder) {
-		return builder
-			.baseUrl(sgisProperties.baseUrl())
-			.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-			.exchangeStrategies(ExchangeStrategies.builder()
-				.codecs(configurer ->
-					configurer.defaultCodecs()
-						.maxInMemorySize(MAX_BUFFER_SIZE)
-				)
-				.build()
-			)
-			.build();
-	}
+  /**
+   * SGIS API 전용 WebClient
+   */
+  @Bean
+  public WebClient sgisApiWebClient(WebClient.Builder builder) {
+    return builder
+        .baseUrl(sgisProperties.baseUrl())
+        .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+        .exchangeStrategies(ExchangeStrategies.builder()
+            .codecs(configurer ->
+                configurer.defaultCodecs()
+                    .maxInMemorySize(MAX_BUFFER_SIZE)
+            )
+            .build()
+        )
+        .build();
+  }
 }
