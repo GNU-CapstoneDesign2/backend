70248366f1ed431f24363cb55a8992d60669134f
Ilang <223224102@gnu.ac.kr>
2025-06-11 15:53:14 +0900

    ✨Feat[#49] ApiResponse로 반환타입 수정

--- src/main/java/org/duckdns/petfinderapp/domain/post/controller/AdoptController.java
+++ src/main/java/org/duckdns/petfinderapp/domain/post/controller/AdoptController.java
@@ -3,6 +3,7 @@ package org.duckdns.petfinderapp.domain.post.controller;
 import lombok.RequiredArgsConstructor;
 import org.duckdns.petfinderapp.domain.post.dto.response.ResAdopt;
 import org.duckdns.petfinderapp.domain.post.service.AdoptService;
+import org.duckdns.petfinderapp.global.template.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class AdoptController {
 
     // Adopt 상세 조회
     @GetMapping("/posts/adopt/{id}")
-    public ResAdopt Apodt(@PathVariable Long id) {
+    public ApiResponse<ResAdopt> apodt(@PathVariable Long id) {
         return adoptService.searchAdopt(id);
     }
 }
--- src/main/java/org/duckdns/petfinderapp/domain/post/service/AdoptService.java
+++ src/main/java/org/duckdns/petfinderapp/domain/post/service/AdoptService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.duckdns.petfinderapp.domain.post.dto.response.ResAdopt;
 import org.duckdns.petfinderapp.domain.post.entity.Adopt;
 import org.duckdns.petfinderapp.domain.post.repository.AdoptRepository;
+import org.duckdns.petfinderapp.global.template.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +15,10 @@ public class AdoptService {
     private final AdoptRepository adoptRepository;
 
     @Transactional(readOnly = true)
-    public ResAdopt searchAdopt(Long id) {
+    public ApiResponse<ResAdopt> searchAdopt(Long id) {
         Adopt adopt = adoptRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다."));
-        return new ResAdopt(adopt);
+        ResAdopt resAdopt = new ResAdopt(adopt);
+        return ApiResponse.onSuccess(HttpStatus.OK, "게시글 조회에 성공했습니다.", resAdopt);
     }
 }
