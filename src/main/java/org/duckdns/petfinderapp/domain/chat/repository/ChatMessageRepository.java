50a5a0551305960afb7587b5f8ef0c05d6af5f90
Algorithm <fighter0628@gmail.com>
2025-06-16 15:21:46 +0900

    :sparkles: Feat[#83]: 메시지 읽기 처리 시 트랜잭션 적용

--- src/main/java/org/duckdns/petfinderapp/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/org/duckdns/petfinderapp/domain/chat/repository/ChatMessageRepository.java
@@ -10,12 +10,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByChatRoomInAndIsReadFalseAndSenderNot(List<ChatRoom> chatRoomList, User sender);
 
     Page<ChatMessage> findAllByChatRoomId(Long roomId, Pageable pageable);
 
+    @Transactional
     @Modifying
     @Query("UPDATE ChatMessage m SET m.isRead = true " +
         "WHERE m.chatRoom.id = :chatRoomId " +
