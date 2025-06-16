963bae4923ecdb12991529185417c0a7e9974781
Algorithm <fighter0628@gmail.com>
2025-06-16 23:40:33 +0900

    ✨ Feat[#62]: Update SecurityConfig to permit access to similarity endpoint

--- src/main/java/org/duckdns/petfinderapp/global/security/SecurityConfig.java
+++ src/main/java/org/duckdns/petfinderapp/global/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
             .requestMatchers(
                 "/auth/login/kakao",
                 "/ws/**",
-                "/posts/**"
+                "/similarity"
             ).permitAll()
             // 그 외의 모든 요청은 인증 필요
             .anyRequest().authenticated()
